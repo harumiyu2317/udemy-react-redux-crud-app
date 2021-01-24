b20efc2950a63ab9b6f3678d1e6494057f057be5
harumiyu2317 <harumiyu2317@gmail.com>
2021-01-24 17:22:19 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React , { Component } from 'react';
 
 function App() {
   return (
-    <h1>Hello World!!</h1>
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am clicked")}} />
+    </React.Fragment>
   );
+  /*
+  const greeting = "Hi!Hi!"
+  const dom = <h1>{greeting}</h1>
+  return dom;
+//  return (
+//    <h1>Hello World!!</h1>
+//  );
+*/
 }
 
 export default App;
