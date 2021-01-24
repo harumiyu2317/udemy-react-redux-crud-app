22f3e7d9c25d8eea773c0f531f5365ef54268b10
harumiyu2317 <harumiyu2317@gmail.com>
2021-01-24 17:43:03 +0900

    jsx compornents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React , { Component } from 'react';
 
+/*
 function App() {
   return (
     <React.Fragment>
@@ -7,14 +8,27 @@ function App() {
       <input type="text" onChange={() => {console.log("I am clicked")}} />
     </React.Fragment>
   );
-  /*
-  const greeting = "Hi!Hi!"
-  const dom = <h1>{greeting}</h1>
-  return dom;
+//  const greeting = "Hi!Hi!"
+//  const dom = <h1>{greeting}</h1>
+//  return dom;
 //  return (
 //    <h1>Hello World!!</h1>
 //  );
+}
 */
+
+const App = () => {
+  return <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+
+  </div>
+}
+
+const Cat = () => {
+  return <div>Meaw!</div>
 }
 
 export default App;
