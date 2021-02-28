b95aab57b09bd5ce38d4b9f04637b062bea14b7d
harumiyu2317 <harumiyu2317@gmail.com>
2021-02-28 21:43:50 +0900

    implement component with connect function

--- src/index.js
+++ src/index.js
@@ -5,17 +5,15 @@ import { Provider } from "react-redux";
 import "./index.css";
 import reducer from "./reducers";
 
-import App from "./compornents/App";
+import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
 const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 
